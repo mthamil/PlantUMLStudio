b603716e02bc33a1ffcea4e5dfa664d53503b731
Matthew Hamilton <matthamilton@live.com>
2012-06-28 01:02:40 -0500

    Added view model for presenting progress info.

---
+++ PlantUmlEditor/ViewModel/IProgressViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace PlantUmlEditor.ViewModel
+{
+	/// <summary>
+	/// Interface for a view model representing progress information for some task.
+	/// </summary>
+	public interface IProgressViewModel
+	{
+		/// <summary>
+		/// Whether there are currently actual discrete chunks of progress.
+		/// </summary>
+		bool HasDiscreteProgress { get; set; }
+
+		/// <summary>
+		/// Whether there is something currently reporting progress.
+		/// </summary>
+		bool InProgress { get; }
+
+		/// <summary>
+		/// The percentage of work completed.
+		/// If null, the percentage is not applicable.
+		/// </summary>
+		int? PercentComplete { get; set; }
+
+		/// <summary>
+		/// The current progress message.
+		/// </summary>
+		string Message { get; set; }
+	}
+}
---
+++ PlantUmlEditor/ViewModel/ProgressViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Utilities.Mvvm;
+using Utilities.PropertyChanged;
+
+namespace PlantUmlEditor.ViewModel
+{
+	/// <summary>
+	/// A class used to represent progress information for tasks.
+	/// </summary>
+	public class ProgressViewModel : ViewModelBase, IProgressViewModel
+	{
+		public ProgressViewModel()
+		{
+			_hasDiscreteProgress = Property.New(this, p => p.HasDiscreteProgress, OnPropertyChanged);
+			_percentComplete = Property.New(this, p => p.PercentComplete, OnPropertyChanged)
+				.AlsoChanges(p => p.InProgress);
+			_message = Property.New(this, p => p.Message, OnPropertyChanged);
+		}
+
+		/// <see cref="IProgressViewModel.HasDiscreteProgress"/>
+		public bool HasDiscreteProgress
+		{
+			get { return _hasDiscreteProgress.Value; }
+			set { _hasDiscreteProgress.Value = value; }
+		}
+
+		/// <see cref="IProgressViewModel.InProgress"/>
+		public bool InProgress
+		{
+			get { return PercentComplete.HasValue; }
+		}
+
+		/// <see cref="IProgressViewModel.PercentComplete"/>
+		public int? PercentComplete
+		{
+			get { return _percentComplete.Value; }
+			set { _percentComplete.Value = value; }
+		}
+
+		/// <see cref="IProgressViewModel.Message"/>
+		public string Message
+		{
+			get { return _message.Value; }
+			set { _message.Value = value; }
+		}
+
+		private readonly Property<bool> _hasDiscreteProgress;
+		private readonly Property<int?> _percentComplete;
+		private readonly Property<string> _message;
+	}
+}

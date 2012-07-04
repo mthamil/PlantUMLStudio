1e16375e097c1e5929ee60d5e15025dc453d2d29
Matthew Hamilton <matthamilton@live.com>
2012-07-04 15:18:14 -0500

    Add synchronous versions of TaskScheduler and SynchronizationContext.

---
+++ Utilities/Concurrency/SynchronousSynchronizationContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace Utilities.Concurrency
+{
+	/// <summary>
+	/// Synchronization context that executes all actions synchronously instead
+	/// of asynchronously.
+	/// </summary>
+	public class SynchronousSynchronizationContext : SynchronizationContext
+	{
+		/// <see cref="SynchronizationContext.Post"/>
+		public override void Post(SendOrPostCallback d, object state)
+		{
+			d(state);
+		}
+	}
+}
---
+++ Utilities/Concurrency/SynchronousTaskScheduler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Utilities.Concurrency
+{
+	/// <summary>
+	/// Implementation of a task scheduler that always executes tasks synchronously
+	/// </summary>
+	public class SynchronousTaskScheduler : TaskScheduler
+	{
+		/// <see cref="TaskScheduler.GetScheduledTasks"/>
+		protected override IEnumerable<Task> GetScheduledTasks()
+		{
+			return Enumerable.Empty<Task>();
+		}
+
+		/// <see cref="TaskScheduler.MaximumConcurrencyLevel"/>
+		public override int MaximumConcurrencyLevel { get { return 1; } }
+
+		/// <see cref="TaskScheduler.QueueTask"/>
+		protected override void QueueTask(Task task)
+		{
+			TryExecuteTask(task);
+		}
+
+		/// <see cref="TaskScheduler.TryExecuteTaskInline"/>
+		protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+		{
+			return TryExecuteTask(task);
+		}
+	}
+}

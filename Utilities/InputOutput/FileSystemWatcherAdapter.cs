c18c82526e6154de454edae0220bb2be49921849
Matthew Hamilton <matthamilton@live.com>
2012-07-13 20:46:11 -0500

    Added an adapter for FileSystemWatcher.

---
+++ Utilities/InputOutput/FileSystemWatcherAdapter.cs
@@ -0,0 +1,100 @@
+using System.IO;
+
+namespace Utilities.InputOutput
+{
+	/// <summary>
+	/// A wrapper around FileSystemWatcher.
+	/// </summary>
+	public class FileSystemWatcherAdapter : IFileSystemWatcher
+	{
+		/// <summary>
+		/// Creates a file system watcher.
+		/// </summary>
+		public FileSystemWatcherAdapter()
+			: this(new FileSystemWatcher()) { }
+
+		/// <summary>
+		/// Creates a file system watcher using an existing watcher.
+		/// </summary>
+		/// <param name="fileSystemWatcher">An existing file system watcher</param>
+		public FileSystemWatcherAdapter(FileSystemWatcher fileSystemWatcher)
+		{
+			_fileSystemWatcher = fileSystemWatcher;
+		}
+
+		/// <summary>
+		/// Gets or sets the path of the directory to watch.
+		/// </summary>
+		/// <returns>
+		/// The path to monitor. The default is an empty string ("").
+		/// </returns>
+		/// <exception cref="T:System.ArgumentException">
+		/// The specified path does not exist or could not be found.-or- 
+		/// The specified path contains wildcard characters.-or- The specified 
+		/// path contains invalid path characters.</exception>
+		public string Path
+		{
+			get { return _fileSystemWatcher.Path; }
+			set { _fileSystemWatcher.Path = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the filter string used to determine what files are monitored in a directory.
+		/// </summary>
+		/// <returns>
+		/// The filter string. The default is "*.*" (Watches all files.)
+		/// </returns>
+		public string Filter
+		{
+			get { return _fileSystemWatcher.Filter; }
+			set { _fileSystemWatcher.Filter = value; }
+		}
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is changed.
+		/// </summary>
+		public event FileSystemEventHandler Changed
+		{
+			add { _fileSystemWatcher.Changed += value; }
+			remove { _fileSystemWatcher.Changed -= value; }
+		}
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is created.
+		/// </summary>
+		public event FileSystemEventHandler Created
+		{
+			add { _fileSystemWatcher.Created += value; }
+			remove { _fileSystemWatcher.Created -= value; }
+		}
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is deleted.
+		/// </summary>
+		public event FileSystemEventHandler Deleted
+		{
+			add { _fileSystemWatcher.Deleted += value; }
+			remove { _fileSystemWatcher.Deleted -= value; }
+		}
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is renamed.
+		/// </summary>
+		public event RenamedEventHandler Renamed
+		{
+			add { _fileSystemWatcher.Renamed += value; }
+			remove { _fileSystemWatcher.Renamed -= value; }
+		}
+
+		/// <summary>
+		/// Occurs when the internal buffer overflows.
+		/// </summary>
+		public event ErrorEventHandler Error
+		{
+			add { _fileSystemWatcher.Error += value; }
+			remove { _fileSystemWatcher.Error -= value; }
+		}
+
+		private readonly FileSystemWatcher _fileSystemWatcher;
+	}
+}
---
+++ Utilities/InputOutput/IFileSystemWatcher.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+
+namespace Utilities.InputOutput
+{
+	/// <summary>
+	/// Interface for a an object that monitors file system changes.
+	/// </summary>
+	public interface IFileSystemWatcher
+	{
+		/// <summary>
+		/// Gets or sets the path of the directory to watch.
+		/// </summary>
+		/// <returns>
+		/// The path to monitor. The default is an empty string ("").
+		/// </returns>
+		/// <exception cref="T:System.ArgumentException">
+		/// The specified path does not exist or could not be found.-or- 
+		/// The specified path contains wildcard characters.-or- The specified 
+		/// path contains invalid path characters.</exception>
+		string Path { get; set; }
+
+		/// <summary>
+		/// Gets or sets the filter string used to determine what files are monitored in a directory.
+		/// </summary>
+		/// <returns>
+		/// The filter string. The default is "*.*" (Watches all files.)
+		/// </returns>
+		string Filter { get; set; }
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is changed.
+		/// </summary>
+		event FileSystemEventHandler Changed;
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is created.
+		/// </summary>
+		event FileSystemEventHandler Created;
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is deleted.
+		/// </summary>
+		event FileSystemEventHandler Deleted;
+
+		/// <summary>
+		/// Occurs when a file or directory in the specified <see cref="P:System.IO.FileSystemWatcher.Path"/> is renamed.
+		/// </summary>
+		event RenamedEventHandler Renamed;
+
+		/// <summary>
+		/// Occurs when the internal buffer overflows.
+		/// </summary>
+		event ErrorEventHandler Error;
+	}
+}

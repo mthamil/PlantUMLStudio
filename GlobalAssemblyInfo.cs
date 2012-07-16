c9528bc97eb2441ca2127353154e6e1f4db114c4
Matthew Hamilton <matthamilton@live.com>
2012-07-16 02:28:28 -0500

    Updated version info and added a common assembly info.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("PlantUML Editor")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.*")]
+[assembly: AssemblyInformationalVersion("2.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif

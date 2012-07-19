fdc4f7139dd8b17a295f1d4f0aa3fea90e131579
Matthew Hamilton <matthamilton@live.com>
2012-07-18 23:09:12 -0500

    Moved class to its own file.

--- Utilities/Concurrency/Processes/ProcessTaskAdapter.cs
+++ Utilities/Concurrency/Processes/ProcessTaskAdapter.cs
@@ -8,39 +8,6 @@ using System.Threading.Tasks;
 namespace Utilities.Concurrency.Processes
 {
 	/// <summary>
-	/// Provides methods that enable using a Process as a Task.
-	/// </summary>
-	public static class ProcessTaskExtensions
-	{
-		/// <summary>
-		/// Creates a Task from a ProcessStartInfo that describes a process that takes a stream
-		/// as input and returns its output.
-		/// </summary>
-		/// <param name="processInfo">Describes the process to execute</param>
-		/// <param name="input">The process input stream</param>
-		/// <param name="cancellationToken">An optional token that can cancel the task</param>
-		/// <returns>A Task representing the process</returns>
-		public static Task<Stream> ToTask(this ProcessStartInfo processInfo, Stream input, CancellationToken cancellationToken)
-		{
-			return ProcessAdapterFactory().Execute(processInfo, input, cancellationToken);
-		}
-
-		/// <summary>
-		/// Creates a Task from a ProcessStartInfo that describes a process that takes
-		/// no input and returns no output.
-		/// </summary>
-		/// <param name="processInfo">Describes the process to execute</param>
-		/// <param name="cancellationToken">An optional token that can cancel the task</param>
-		/// <returns>A Task representing the process</returns>
-		public static Task ToTask(this ProcessStartInfo processInfo, CancellationToken cancellationToken)
-		{
-			return ProcessAdapterFactory().Execute(processInfo, cancellationToken);
-		}
-
-		internal static Func<IProcessTaskAdapter> ProcessAdapterFactory = () => new ProcessTaskAdapter(TaskScheduler.Default);
-	}
-
-	/// <summary>
 	/// Executes Processes and returns Task representations of them.
 	/// </summary>
 	public class ProcessTaskAdapter : IProcessTaskAdapter

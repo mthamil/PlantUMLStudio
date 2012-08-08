43ae223c1138132b6f9505d100f040a62462272f
Matthew Hamilton <matthamilton@live.com>
2012-08-08 16:26:00 -0500

    Updated design time data.

--- PlantUmlEditor/DesignTimeData/DiagramFiles.cs
+++ PlantUmlEditor/DesignTimeData/DiagramFiles.cs
@@ -10,67 +10,43 @@ namespace PlantUmlEditor.DesignTimeData
         {
             this.Add(new Diagram()
             {
-                Content = 
-@"@startuml cpe.png
-actor EndUser
-participant SaaS
-participant CPE
-
-EndUser -> SaaS : Click Download
-activate SaaS
-	SaaS -> CPE: Generate Unique URL
-	CPE -> SaaS: Unique URL
-	SaaS -> EndUser: Unique URL
-deactivate SaaS
-
-EndUser -> CPE : Hit Unique URL
-activate CPE
-	CPE -> SaaS : Validate Unique URL
-	activate SaaS
-		SaaS -> SaaS : Mark Unique URL as disposed
-		SaaS -> CPE : Actual App Url
-	deactivate SaaS
-	CPE -> CPE : Read the file from actual URL
-	CPE -> EndUser : Transmit actual app binary
-deactivate CPE
+                Content =
+@"@startuml class.png
+package ""Sample Package""
+  namespace N1 {
+    class C1 {
+      Method1()
+    }
+    enum E1 {
+      Item1
+      Item2
+      Item3
+    }
+  }
+end package
 @enduml",
-                File = new FileInfo("test.txt"),
+                File = new FileInfo("class.txt"),
                 //ImageFilePath = "http://plantuml.sourceforge.net/img/sequence_img009.png"
             });
 
             this.Add(new Diagram()
             {
                 Content = 
-@"@startuml btconnectjourney.png
-actor User
-participant AOOJ
-participant SaaS
-participant DnP
-participant C2B
-participant HE
-
-User -> AOOJ: Enters userID 
-activate AOOJ
-	AOOJ -> DnP: Check for uniqueness
-	DnP -> AOOJ: Unique, no problem	
-deactivate AOOJ
-
-User -> AOOJ: Submit order
-activate AOOJ
-	AOOJ -> SaaS: Queue Order
-	AOOJ --> User: Show order in progress
-deactivate AOOJ
-
-SaaS -> SaaS: Process order
-activate SaaS
-	SaaS -> DnP: Create user account
-	SaaS -> C2B: Create customer account
-	SaaS -> HE: Create mailbox
-	SaaS -> User: Send welcome email
-deactivate SaaS
-
+@"@startuml sequence.png
+participant User
+User -> A: DoWork
+activate A
+  A -> A: Internal call
+  activate A
+    A -> B: << createRequest >>
+    activate B
+      B --> A: RequestCreated
+    deactivate B
+  deactivate A
+  A -> User: Done
+deactivate A
 @enduml",
-                File = new FileInfo("test2.txt"),
+                File = new FileInfo("sequence.txt"),
                // ImageFilePath = "http://plantuml.sourceforge.net/img/activity_img06.png"
             });
         }

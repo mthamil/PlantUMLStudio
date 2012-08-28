c900725a7e5b19539db70e804cd8b6ba28c98b93
Matthew Hamilton <matthamilton@live.com>
2012-08-28 14:29:52 -0500

    Renamed type parameters.

--- Utilities/WeakEventHandler.cs
+++ Utilities/WeakEventHandler.cs
@@ -10,31 +10,31 @@ namespace Utilities
 	/// <summary>
 	/// Interface for a weak event handler.
 	/// </summary>
-	/// <typeparam name="E">The event args type</typeparam>
-	public interface IWeakEventHandler<E> where E : EventArgs
+	/// <typeparam name="TArgs">The event args type</typeparam>
+	public interface IWeakEventHandler<TArgs> where TArgs : EventArgs
 	{
 		/// <summary>
 		/// The wrapped event handler.
 		/// </summary>
-		EventHandler<E> Handler { get; }
+		EventHandler<TArgs> Handler { get; }
 	}
 
 	/// <summary>
 	/// A weak event handler whose target can be garbage collected without event unsubscription
 	/// once it becoems unreachable.
 	/// </summary>
-	/// <typeparam name="T">The handler declaring type</typeparam>
-	/// <typeparam name="E">The event args type</typeparam>
-	public class WeakEventHandler<T, E> : IWeakEventHandler<E>
-		where T : class
-		where E : EventArgs
+	/// <typeparam name="TTarget">The handler declaring type</typeparam>
+	/// <typeparam name="TArgs">The event args type</typeparam>
+	public class WeakEventHandler<TTarget, TArgs> : IWeakEventHandler<TArgs>
+		where TTarget : class
+		where TArgs : EventArgs
 	{
 		/// <summary>
 		/// Creates a new weak event handler.
 		/// </summary>
 		/// <param name="eventHandler">The event handler to wrap</param>
 		/// <param name="unregister">A callback that will unsubscribe the handler once its target is garbage collected</param>
-		public WeakEventHandler(EventHandler<E> eventHandler, UnregisterCallback<E> unregister)
+		public WeakEventHandler(EventHandler<TArgs> eventHandler, UnregisterCallback<TArgs> unregister)
 		{
 			_targetRef = new WeakReference(eventHandler.Target);
 			_openHandler = (OpenEventHandler)Delegate.CreateDelegate(openEventHandlerType, null, eventHandler.Method);
@@ -47,9 +47,9 @@ namespace Utilities
 		/// </summary>
 		/// <param name="sender">The event sender</param>
 		/// <param name="e">The event args</param>
-		public void Invoke(object sender, E e)
+		public void Invoke(object sender, TArgs e)
 		{
-			T target = (T)_targetRef.Target;
+			TTarget target = (TTarget)_targetRef.Target;
 
 			if (target != null)
 				_openHandler.Invoke(target, sender, e);
@@ -63,7 +63,7 @@ namespace Utilities
 		#region Implementation of IWeakEventHandler<E>
 
 		/// <see cref="IWeakEventHandler{E}.Handler"/>
-		public EventHandler<E> Handler
+		public EventHandler<TArgs> Handler
 		{
 			get { return _handler; }
 		}
@@ -73,17 +73,17 @@ namespace Utilities
 		/// <summary>
 		/// Converts a weak event handler to a regular event handler.
 		/// </summary>
-		public static implicit operator EventHandler<E>(WeakEventHandler<T, E> weakHandler)
+		public static implicit operator EventHandler<TArgs>(WeakEventHandler<TTarget, TArgs> weakHandler)
 		{
 			return weakHandler._handler;
 		}
 
-		private delegate void OpenEventHandler(T @this, object sender, E e);
+		private delegate void OpenEventHandler(TTarget @this, object sender, TArgs e);
 
 		private readonly WeakReference _targetRef;
 		private readonly OpenEventHandler _openHandler;
-		private readonly EventHandler<E> _handler;
-		private UnregisterCallback<E> _unregister;
+		private readonly EventHandler<TArgs> _handler;
+		private UnregisterCallback<TArgs> _unregister;
 
 		private static readonly Type openEventHandlerType = typeof(OpenEventHandler);
 	}
@@ -96,12 +96,12 @@ namespace Utilities
 		/// <summary>
 		/// Wraps an event handler in a weak event handler.
 		/// </summary>
-		/// <typeparam name="E">The event args type</typeparam>
+		/// <typeparam name="TArgs">The event args type</typeparam>
 		/// <param name="eventHandler">The event handler to wrap</param>
 		/// <param name="unregister">A callback that will unsubscribe the handler once its target is garbage collected</param>
 		/// <returns>A weak event handler</returns>
-		public static EventHandler<E> MakeWeak<E>(this EventHandler<E> eventHandler, UnregisterCallback<E> unregister)
-			where E : EventArgs
+		public static EventHandler<TArgs> MakeWeak<TArgs>(this EventHandler<TArgs> eventHandler, UnregisterCallback<TArgs> unregister)
+			where TArgs : EventArgs
 		{
 			if (eventHandler == null)
 				throw new ArgumentNullException("eventHandler");
@@ -109,10 +109,10 @@ namespace Utilities
 			if (eventHandler.Method.IsStatic || eventHandler.Target == null)
 				throw new ArgumentException(@"Only instance methods are supported.", "eventHandler");
 
-			var closedWeakHandlerType = openWeakEventHandlerType.MakeGenericType(eventHandler.Method.DeclaringType, typeof(E));
-			var handlerConstructor = closedWeakHandlerType.GetConstructor(new[] { typeof(EventHandler<E>), typeof(UnregisterCallback<E>) });
+			var closedWeakHandlerType = openWeakEventHandlerType.MakeGenericType(eventHandler.Method.DeclaringType, typeof(TArgs));
+			var handlerConstructor = closedWeakHandlerType.GetConstructor(new[] { typeof(EventHandler<TArgs>), typeof(UnregisterCallback<TArgs>) });
 
-			var weakEventHandler = (IWeakEventHandler<E>)handlerConstructor.Invoke(new object[] { eventHandler, unregister });
+			var weakEventHandler = (IWeakEventHandler<TArgs>)handlerConstructor.Invoke(new object[] { eventHandler, unregister });
 
 			return weakEventHandler.Handler;
 		}

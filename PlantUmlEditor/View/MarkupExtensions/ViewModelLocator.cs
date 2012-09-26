32939bdfb962b4b5c5f7140327f01bc49c0fe4b2
Matthew Hamilton <matthamilton@live.com>
2012-09-26 14:45:32 -0500

    - Created markup extension that allows instantiation of types using generic type arguments and/or constructor arguments. - Refactored ViewModelLocator to be based on this markup extension.

--- PlantUmlEditor/View/MarkupExtensions/ViewModelLocator.cs
+++ PlantUmlEditor/View/MarkupExtensions/ViewModelLocator.cs
@@ -1,46 +1,28 @@
 using System;
 using System.Windows.Markup;
 using PlantUmlEditor.ViewModel;
-using Utilities.Mvvm;
+using Utilities.Controls.Markup;
 
 namespace PlantUmlEditor.View.MarkupExtensions
 {
 	/// <summary>
 	/// A XAML markup extension to allow definition of a generic view-model locator.
 	/// </summary>
-	public class ViewModelLocator : MarkupExtension
+	public class ViewModelLocator : ActivatorExtension
 	{
-		public ViewModelLocator(Type typeArgument, string name)
-		{
-			TypeArgument = typeArgument;
-			Name = name;
-		}
-
-		public ViewModelLocator() : this(typeof(ViewModelBase), "") { }
-
 		#region Overrides of MarkupExtension
 
 		/// <see cref="MarkupExtension.ProvideValue"/>
 		public override object ProvideValue(IServiceProvider serviceProvider)
 		{
-			return Locator;
+			Type = viewModelLocatorType;
+			TypeArguments.Add(TypeArgument);
+			ConstructorArguments.Add(App.Container);
+			ConstructorArguments.Add(Name);
+			return base.ProvideValue(serviceProvider);
 		}
 
 		#endregion
-
-		/// <summary>
-		/// The actual value returned from ProvideValue.
-		/// </summary>
-		public object Locator
-		{
-			get
-			{
-				if (_viewModelLocator == null)
-					_viewModelLocator = CreateViewModelLocator();
-
-				return _viewModelLocator;
-			}
-		}
 
 		/// <summary>
 		/// The type of the view-model.
@@ -48,21 +30,10 @@ namespace PlantUmlEditor.View.MarkupExtensions
 		public Type TypeArgument { get; set; }
 
 		/// <summary>
-		/// The object config name of a Spring-defined view-model.
+		/// The object config name of a container-defined view-model.
 		/// </summary>
 		public string Name { get; set; }
 
-		private object CreateViewModelLocator()
-		{
-			return Activator.CreateInstance(ConstructViewModelLocatorType(), App.Container, Name);
-		}
-
-		private Type ConstructViewModelLocatorType()
-		{
-			return viewModelLocatorType.MakeGenericType(TypeArgument);
-		}
-
-		private object _viewModelLocator;
 		private static readonly Type viewModelLocatorType = typeof(AutofacViewModelLocator<>);
 	}
 }
---
+++ Utilities.Controls/Markup/ActivatorExtension.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace Utilities.Controls.Markup
+{
+	/// <summary>
+	/// A XAML markup extension to allow creating instances of types using
+	/// generic type arguments and non-default constructors.
+	/// </summary>
+	public class ActivatorExtension : MarkupExtension
+	{
+		/// <summary>
+		/// Initializes the extension.
+		/// </summary>
+		public ActivatorExtension()
+		{
+			_instance = new Lazy<object>(() => 
+				Activator.CreateInstance(ConstructType(), ConstructorArguments.ToArray()));
+		}
+
+		#region Overrides of MarkupExtension
+
+		/// <see cref="MarkupExtension.ProvideValue"/>
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return _instance.Value;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// The type to instantiate.
+		/// </summary>
+		public Type Type { get; set; }
+
+		/// <summary>
+		/// The type arguments of the type to instantiate.
+		/// </summary>
+		public Collection<Type> TypeArguments
+		{
+			get { return _typeArguments; } 
+		}
+
+		/// <summary>
+		/// The arguments to instantiate an instance with.
+		/// </summary>
+		public Collection<object> ConstructorArguments
+		{
+			get { return _constructorArguments; }
+		}
+
+		private Type ConstructType()
+		{
+			return Type.IsGenericTypeDefinition 
+				? Type.MakeGenericType(TypeArguments.ToArray()) 
+				: Type;
+		}
+
+		private readonly Lazy<object> _instance;
+		private readonly Collection<Type> _typeArguments = new Collection<Type>();
+		private readonly Collection<object> _constructorArguments = new Collection<object>(); 
+	}
+}

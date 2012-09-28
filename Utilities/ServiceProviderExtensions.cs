956a969fa8a77b2ae7838ca87084bcd89f7f3a5a
Matthew Hamilton <matthamilton@live.com>
2012-09-28 16:36:04 -0500

    Added extension method to avoid repetitive casting.

---
+++ Utilities/ServiceProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Utilities
+{
+	/// <summary>
+	/// Provides extension methods for IServiceProvider.
+	/// </summary>
+	public static class ServiceProviderExtensions
+	{
+		/// <summary>
+		/// Gets the service of a specified type.
+		/// </summary>
+		/// <typeparam name="TService">The type of service</typeparam>
+		/// <param name="serviceProvider">The service provider</param>
+		/// <returns>A service of the specified type</returns>
+		public static TService GetService<TService>(this IServiceProvider serviceProvider)
+		{
+			return (TService)serviceProvider.GetService(typeof(TService));
+		}
+	}
+}

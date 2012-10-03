52210d8a9e2901a99bd360abb3d439beaae61bbe
Matthew Hamilton <matthamilton@live.com>
2012-10-02 19:21:43 -0500

    Added tests for NegatingConverter.

---
+++ Unit.Tests/Utilities.Controls/Converters/NegatingConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Utilities.Controls.Converters;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Unit.Tests.Utilities.Controls.Converters
+{
+	public class NegatingConverterTests
+	{
+		[Theory]
+		[InlineData(true, false)]
+		[InlineData(false, true)]
+		public void Test_Convert(bool expected, bool input)
+		{
+			// Act.
+			bool actual = (bool)converter.Convert(input, typeof(bool), null, CultureInfo.InvariantCulture);
+
+			// Assert.
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData(true, false)]
+		[InlineData(false, true)]
+		public void Test_ConvertBack(bool expected, bool input)
+		{
+			// Act.
+			bool actual = (bool)converter.ConvertBack(input, typeof(bool), null, CultureInfo.InvariantCulture);
+
+			// Assert.
+			Assert.Equal(expected, actual);
+		}
+
+		private readonly NegatingConverter converter = new NegatingConverter();
+	}
+}

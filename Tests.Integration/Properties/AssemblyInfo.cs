b6534545d1333f6860a17e2aee3ea14ce10e6c37
Matthew Hamilton <matthamilton@live.com>
2013-01-31 17:45:05 -0600

    Added integration test project.

---
+++ Tests.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests.Integration")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a4f05c7-24f8-4a86-8e15-8c1f6a5d2c8f")]

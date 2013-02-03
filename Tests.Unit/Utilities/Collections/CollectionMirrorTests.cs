15cfcfb583096165acf613b8a0f2aeae09dd215e
Matthew Hamilton <matthamilton@live.com>
2013-02-03 00:33:48 -0600

    Added collection mirror.

---
+++ Tests.Unit/Utilities/Collections/CollectionMirrorTests.cs
@@ -0,0 +1,617 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Moq;
+using Utilities.Collections;
+using Xunit;
+
+namespace Tests.Unit.Utilities.Collections
+{
+	public class CollectionMirrorTests
+	{
+		[Fact]
+		public void Test_InitialSynchronization()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string> { "4", "5", "6" };
+
+			// Act.
+			var mirror = new CollectionMirror(source, target);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_AddToSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string>();
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.Add("1");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1" }, source);
+			AssertThat.SequenceEqual(new[] { "1" }, target);
+		}
+
+		[Fact]
+		public void Test_AddToTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string>();
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.Add("1");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1" }, source);
+			AssertThat.SequenceEqual(new[] { "1" }, target);
+		}
+
+		[Fact]
+		public void Test_InsertToSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.Insert(1, "2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_InsertToTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.Insert(1, "2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveFromSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.Remove("2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveFromTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.Remove("2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveIndexFromSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.RemoveAt(2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "2" }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveIndexFromTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.RemoveAt(2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2" }, source);
+			AssertThat.SequenceEqual(new[] { "1", "2" }, target);
+		}
+
+		[Fact]
+		public void Test_ReplaceInSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source[0] = "0";
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_ReplaceInTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target[0] = "0";
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_MoveInSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.Move(0, 2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, source);
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, target);
+		}
+
+		[Fact]
+		public void Test_MoveInTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.Move(0, 2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, source);
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, target);
+		}
+
+		[Fact]
+		public void Test_ClearSource()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			source.Clear();
+
+			// Assert.
+			AssertThat.SequenceEqual(new string[0], source);
+			AssertThat.SequenceEqual(new string[0], target);
+		}
+
+		[Fact]
+		public void Test_ClearTarget()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			target.Clear();
+
+			// Assert.
+			AssertThat.SequenceEqual(new string[0], source);
+			AssertThat.SequenceEqual(new string[0], target);
+		}
+
+		[Fact]
+		public void Test_RemoveMultiple()
+		{
+			// Arrange.
+			var source = new Mock<IObservableList> { DefaultValue = DefaultValue.Empty };
+			source.Setup(s => s.GetEnumerator()).Returns(new [] { "1", "2", "3" }.GetEnumerator());
+
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source.Object, target);
+
+			// Act.
+			source.Raise(s => s.CollectionChanged += null, 
+				new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, new [] { "2", "3" }, 1));
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1" }, target);
+		}
+
+		[Fact]
+		public void Test_AddMultiple()
+		{
+			// Arrange.
+			var source = new Mock<IObservableList> { DefaultValue = DefaultValue.Empty };
+			source.Setup(s => s.GetEnumerator()).Returns(new[] { "1", "2", "3" }.GetEnumerator());
+
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source.Object, target);
+
+			// Act.
+			source.Raise(s => s.CollectionChanged += null,
+				new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, new[] { "4", "5" }, 3));
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3", "4", "5" }, target);
+		}
+
+		[Fact]
+		public void Test_Dispose_Unsubscribes()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<string>();
+			var mirror = new CollectionMirror(source, target);
+
+			// Act.
+			mirror.Dispose();
+			source.Add("4");
+			target.Remove("1");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3", "4" }, source);
+			AssertThat.SequenceEqual(new[] { "2", "3" }, target);
+		}
+
+		[Fact]
+		public void Test_InitialSynchronization_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int> { 4, 5, 6 };
+
+			// Act.
+			var mirror = new CollectionMirror(source, target, 
+				obj => Int32.Parse((string)obj), 
+				obj => obj.ToString());
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 2, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_AddToSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string>();
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Add("1");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1" }, source);
+			AssertThat.SequenceEqual(new[] { 1 }, target);
+		}
+
+		[Fact]
+		public void Test_AddToTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string>();
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.Add(1);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1" }, source);
+			AssertThat.SequenceEqual(new[] { 1 }, target);
+		}
+
+		[Fact]
+		public void Test_InsertToSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Insert(1, "2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 2, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_InsertToTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.Insert(1, 2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 2, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveFromSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Remove("2");
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveFromTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.Remove(2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveIndexFromSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.RemoveAt(2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 2 }, target);
+		}
+
+		[Fact]
+		public void Test_RemoveIndexFromTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.RemoveAt(2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "1", "2" }, source);
+			AssertThat.SequenceEqual(new[] { 1, 2 }, target);
+		}
+
+		[Fact]
+		public void Test_ReplaceInSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source[0] = "0";
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 0, 2, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_ReplaceInTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target[0] = 0;
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "0", "2", "3" }, source);
+			AssertThat.SequenceEqual(new[] { 0, 2, 3 }, target);
+		}
+
+		[Fact]
+		public void Test_MoveInSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Move(0, 2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, source);
+			AssertThat.SequenceEqual(new[] { 2, 3, 1 }, target);
+		}
+
+		[Fact]
+		public void Test_MoveInTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.Move(0, 2);
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { "2", "3", "1" }, source);
+			AssertThat.SequenceEqual(new[] { 2, 3, 1 }, target);
+		}
+
+		[Fact]
+		public void Test_ClearSource_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Clear();
+
+			// Assert.
+			AssertThat.SequenceEqual(new string[0], source);
+			AssertThat.SequenceEqual(new int[0], target);
+		}
+
+		[Fact]
+		public void Test_ClearTarget_WithMapping()
+		{
+			// Arrange.
+			var source = new ObservableCollection<string> { "1", "2", "3" };
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			target.Clear();
+
+			// Assert.
+			AssertThat.SequenceEqual(new string[0], source);
+			AssertThat.SequenceEqual(new int[0], target);
+		}
+
+		[Fact]
+		public void Test_RemoveMultiple_WithMapping()
+		{
+			// Arrange.
+			var source = new Mock<IObservableList> { DefaultValue = DefaultValue.Empty };
+			source.Setup(s => s.GetEnumerator()).Returns(new[] { "1", "2", "3" }.GetEnumerator());
+
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source.Object, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Raise(s => s.CollectionChanged += null,
+				new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, new[] { "2", "3" }, 1));
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { 1 }, target);
+		}
+
+		[Fact]
+		public void Test_AddMultiple_WithMapping()
+		{
+			// Arrange.
+			var source = new Mock<IObservableList> { DefaultValue = DefaultValue.Empty };
+			source.Setup(s => s.GetEnumerator()).Returns(new[] { "1", "2", "3" }.GetEnumerator());
+
+			var target = new ObservableCollection<int>();
+			var mirror = new CollectionMirror(source.Object, target,
+				obj => Int32.Parse((string)obj),
+				obj => obj.ToString());
+
+			// Act.
+			source.Raise(s => s.CollectionChanged += null,
+				new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, new[] { "4", "5" }, 3));
+
+			// Assert.
+			AssertThat.SequenceEqual(new[] { 1, 2, 3, 4, 5 }, target);
+		}
+
+		public interface IObservableList : INotifyCollectionChanged, IList { }
+	}
+}
---
+++ Utilities/Collections/CollectionMirror.cs
@@ -0,0 +1,219 @@
+﻿//  PlantUML Editor
+//  Copyright 2013 Matthew Hamilton - matthamilton@live.com
+//  Copyright 2010 Omar Al Zabir - http://omaralzabir.com/ (original author)
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Windows;
+
+namespace Utilities.Collections
+{
+	/// <summary>
+	/// Synchronizes two collections that implement INotifyCollectionChanged.
+	/// </summary>
+	public class CollectionMirror : DisposableBase, IWeakEventListener
+	{
+		/// <summary>
+		/// Initializes a new collection mirror.
+		/// </summary>
+		/// <param name="source">The source collection</param>
+		/// <param name="target">The target collection</param>
+		public CollectionMirror(IList source, IList target)
+			: this(source, target, obj => obj, obj => obj) { }
+
+		/// <summary>
+		/// Initializes a new collection mirror with transformations.
+		/// </summary>
+		/// <param name="source">The source collection</param>
+		/// <param name="target">The target collection</param>
+		/// <param name="sourceToTarget">A mapping from source items to target items</param>
+		/// <param name="targetToSource">A mapping from target items to source items</param>
+		public CollectionMirror(IList source, IList target, Func<object, object> sourceToTarget, Func<object, object> targetToSource)
+		{
+			_source = source;
+			_target = target;
+			_sourceToTarget = sourceToTarget;
+			_targetToSource = targetToSource;
+
+			Synchronize(_source, _target, _sourceToTarget);
+			Subscribe(_source);
+		}
+
+		private void Unsubscribe(ICollection collection)
+		{
+			var notifierCollection = collection as INotifyCollectionChanged;
+			if (notifierCollection != null)
+				CollectionChangedEventManager.RemoveListener(notifierCollection, this);
+		}
+
+		private void Subscribe(ICollection collection)
+		{
+			var notifierCollection = collection as INotifyCollectionChanged;
+			if (notifierCollection != null)
+				CollectionChangedEventManager.AddListener(notifierCollection, this);
+		}
+
+		/// <summary>
+		/// Ensures that the target collection matches the source collection.
+		/// </summary>
+		/// <remarks>The current implementation first clears the target, removing all existing items</remarks>
+		private void Synchronize(IList source, IList target, Func<object, object> mapping)
+		{
+			using (SuppressChangeEvents(target))
+			{
+				target.Clear();
+				foreach (var item in source)
+					target.Add(mapping(item));
+			}
+		}
+
+		private void PropagateAdd(IList collection, Func<object, object> mapping, NotifyCollectionChangedEventArgs args)
+		{
+			using (SuppressChangeEvents(collection))
+			{
+				for (int i = 0; i < args.NewItems.Count; i++)
+				{
+					int insertionIndex = i + args.NewStartingIndex;
+					var newItem = args.NewItems[i];
+
+					if (insertionIndex > collection.Count)
+						collection.Add(mapping(newItem));
+					else
+						collection.Insert(insertionIndex, mapping(newItem));
+				}
+			}
+		}
+
+		private void PropagateRemove(IList collection, NotifyCollectionChangedEventArgs args)
+		{
+			using (SuppressChangeEvents(collection))
+			{
+				for (int i = args.OldItems.Count - 1; i >= 0; i--)
+				{
+					int removalIndex = i + args.OldStartingIndex;
+					collection.RemoveAt(removalIndex);
+				}
+			}
+		}
+
+		private void PropagateReplace(IList collection, Func<object, object> mapping, NotifyCollectionChangedEventArgs args)
+		{
+			using (SuppressChangeEvents(collection))
+			{
+				for (int i = 0; i < args.NewItems.Count; i++)
+				{
+					int replacementIndex = i + args.NewStartingIndex;
+					collection[replacementIndex] = mapping(args.NewItems[replacementIndex]);
+				}
+			}
+		}
+
+		private void PropagateMove(IList collection, Func<object, object> mapping, NotifyCollectionChangedEventArgs args)
+		{
+			PropagateRemove(collection, args);
+			PropagateAdd(collection, mapping, args);
+		}
+
+		private void PropagateReset(IList collection)
+		{
+			var source = collection == _source ? _target : _source;
+			var mapping = collection == source ? _sourceToTarget : _targetToSource;
+			Synchronize(source, collection, mapping);
+		}
+
+		#region Implementation of IWeakEventListener
+
+		/// <see cref="IWeakEventListener.ReceiveWeakEvent"/>
+		public bool ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
+		{
+			var changeArgs = (NotifyCollectionChangedEventArgs)e;
+			var changedCollection = (IList)sender;
+
+			var collectionToUpdate = changedCollection == _source ? _target : _source;
+			var mapping = changedCollection == _source ? _sourceToTarget : _targetToSource;
+
+			switch (changeArgs.Action)
+			{
+				case NotifyCollectionChangedAction.Add:
+					PropagateAdd(collectionToUpdate, mapping, changeArgs);
+					break;
+				case NotifyCollectionChangedAction.Remove:
+					PropagateRemove(collectionToUpdate, changeArgs);
+					break;
+				case NotifyCollectionChangedAction.Replace:
+					PropagateReplace(collectionToUpdate, mapping, changeArgs);
+					break;
+				case NotifyCollectionChangedAction.Move:
+					PropagateMove(collectionToUpdate, mapping, changeArgs);
+					break;
+				case NotifyCollectionChangedAction.Reset:
+					PropagateReset(collectionToUpdate);
+					break;
+			}
+
+			return true;
+		}
+
+		#endregion
+
+		#region Implementation of DisposableBase
+
+		/// <see cref="DisposableBase.OnDisposing"/>
+		protected override void OnDisposing()
+		{
+			Unsubscribe(_source);
+			Unsubscribe(_target);
+		}
+
+		#endregion
+
+		private readonly IList _source;
+		private readonly IList _target;
+		private readonly Func<object, object> _sourceToTarget;
+		private readonly Func<object, object> _targetToSource;
+
+		/// <summary>
+		/// Temporarily suppresses handling of change events for a collection.
+		/// </summary>
+		private IDisposable SuppressChangeEvents(ICollection collection)
+		{
+			return new ChangeEventSuppressionToken(collection, this);
+		}
+
+		private class ChangeEventSuppressionToken : IDisposable
+		{
+			public ChangeEventSuppressionToken(ICollection collection, CollectionMirror listener)
+			{
+				_collection = collection;
+				_listener = listener;
+
+				_listener.Unsubscribe(_collection);
+			}
+
+			#region Implementation of IDisposable
+
+			public void Dispose()
+			{
+				_listener.Subscribe(_collection);
+			}
+
+			#endregion
+
+			private readonly ICollection _collection;
+			private readonly CollectionMirror _listener;
+		}
+	}
+}

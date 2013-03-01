062b463d07124691cbab7cd8a6c412036cdd8f3d
Matthew Hamilton <matthamilton@live.com>
2013-02-28 20:54:09 -0600

    Fixed indentation alignment.

--- PlantUmlEditor/Container/CoreModule.cs
+++ PlantUmlEditor/Container/CoreModule.cs
@@ -45,52 +45,52 @@ namespace PlantUmlEditor.Container
 			builder.RegisterType<SystemTimer>().As<ITimer>();
 
 			builder.RegisterType<SystemClock>().As<IClock>()
-				.SingleInstance();
+			       .SingleInstance();
 
 			builder.Register(c => new DotNetSettings(
-					Settings.Default,
-					new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"PlantUmlEditor\samples\"))))
-				.As<ISettings>()
-				.SingleInstance();
+				        Settings.Default,
+				        new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"PlantUmlEditor\samples\"))))
+			       .As<ISettings>()
+			       .SingleInstance();
 
 			builder.RegisterType<FileSystemWatcherAdapter>().As<IFileSystemWatcher>();
 			builder.RegisterType<DirectoryMonitor>().As<IDirectoryMonitor>()
-				.WithProperty(p => p.FileCreationWaitTimeout, TimeSpan.FromSeconds(2))
-				.OnActivating(c => c.Instance.Filter = "*" + c.Context.Resolve<ISettings>().DiagramFileExtension);
+			       .WithProperty(p => p.FileCreationWaitTimeout, TimeSpan.FromSeconds(2))
+			       .OnActivating(c => c.Instance.Filter = "*" + c.Context.Resolve<ISettings>().DiagramFileExtension);
 
 			builder.RegisterType<DiagramBitmapRenderer>().As<IDiagramRenderer>();
 
 			builder.RegisterType<GraphViz>().As<IExternalComponent>()
-				.OnActivating(c =>
-				{
-					c.Instance.GraphVizExecutable = c.Context.Resolve<ISettings>().GraphVizExecutable;
-					c.Instance.LocalVersionMatchingPattern = c.Context.Resolve<ISettings>().GraphVizLocalVersionPattern;
-				});
+			       .OnActivating(c =>
+			       {
+				       c.Instance.GraphVizExecutable = c.Context.Resolve<ISettings>().GraphVizExecutable;
+				       c.Instance.LocalVersionMatchingPattern = c.Context.Resolve<ISettings>().GraphVizLocalVersionPattern;
+			       });
 
 			builder.RegisterType<PlantUml>().As<IDiagramCompiler, IExternalComponent>()
-				.OnActivating(c =>
-				{
-					c.Instance.PlantUmlJar = c.Context.Resolve<ISettings>().PlantUmlJar;
-					c.Instance.GraphVizExecutable = c.Context.Resolve<ISettings>().GraphVizExecutable;
-					c.Instance.LocalVersionMatchingPattern = c.Context.Resolve<ISettings>().PlantUmlLocalVersionPattern;
-					c.Instance.LocalLocation = c.Context.Resolve<ISettings>().PlantUmlJar;
-					c.Instance.RemoteLocation = c.Context.Resolve<ISettings>().PlantUmlDownloadLocation;
-					c.Instance.VersionLocation = c.Context.Resolve<ISettings>().PlantUmlVersionSource;
-					c.Instance.RemoteVersionMatchingPattern = c.Context.Resolve<ISettings>().PlantUmlRemoteVersionPattern;
-				});
+			       .OnActivating(c =>
+			       {
+				       c.Instance.PlantUmlJar = c.Context.Resolve<ISettings>().PlantUmlJar;
+				       c.Instance.GraphVizExecutable = c.Context.Resolve<ISettings>().GraphVizExecutable;
+				       c.Instance.LocalVersionMatchingPattern = c.Context.Resolve<ISettings>().PlantUmlLocalVersionPattern;
+				       c.Instance.LocalLocation = c.Context.Resolve<ISettings>().PlantUmlJar;
+				       c.Instance.RemoteLocation = c.Context.Resolve<ISettings>().PlantUmlDownloadLocation;
+				       c.Instance.VersionLocation = c.Context.Resolve<ISettings>().PlantUmlVersionSource;
+				       c.Instance.RemoteVersionMatchingPattern = c.Context.Resolve<ISettings>().PlantUmlRemoteVersionPattern;
+			       });
 
 			builder.RegisterType<DiagramIOService>().As<IDiagramIOService>()
-				.OnActivating(c => c.Instance.FileFilter = "*" + c.Context.Resolve<ISettings>().DiagramFileExtension);
+			       .OnActivating(c => c.Instance.FileFilter = "*" + c.Context.Resolve<ISettings>().DiagramFileExtension);
 
 			builder.RegisterType<SnippetParser>().As<ISnippetParser>();
 
 			builder.RegisterType<SnippetProvider>()
-				.WithProperty(p => p.SnippetLocation, new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"snippets\")))
-				.OnActivating(c => c.Instance.Load())
-				.SingleInstance();
+			       .WithProperty(p => p.SnippetLocation, new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"snippets\")))
+			       .OnActivating(c => c.Instance.Load())
+			       .SingleInstance();
 
 			builder.RegisterType<WindowsSecurityService>().As<ISecurityService>()
-				.SingleInstance();
+			       .SingleInstance();
 		}
 	}
 }

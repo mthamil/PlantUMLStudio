001aeb2151623771be4d16c076923b6318b5072f
Matthew Hamilton <matthamilton@live.com>
2014-09-24 14:59:52 -0500

    Updated tests for file system info comparison.

--- Tests.Unit/Utilities/InputOutput/FileSystemInfoPathEqualityComparerTests.cs
+++ Tests.Unit/Utilities/InputOutput/FileSystemInfoPathEqualityComparerTests.cs
@@ -6,28 +6,29 @@ using Xunit.Extensions;
 
 namespace Tests.Unit.Utilities.InputOutput
 {
-	public class FileInfoPathEqualityComparerTests
+	public class FileSystemInfoPathEqualityComparerTests
 	{
 		public static IEnumerable<object[]> EqualsData
 		{
 			get
 			{
 				var sameFile = new FileInfo(@"c:\test.txt");
-				return new TheoryDataSet<bool, FileInfo, FileInfo>
+				return new TheoryDataSet<bool, FileSystemInfo, FileSystemInfo>
 				{
 					{ true,  sameFile, sameFile },
-					{ true,  new FileInfo(@"c:\test.txt"), new FileInfo(@"c:\test.txt") },
-					{ true,  new FileInfo(@"c:\test.txt"), new FileInfo(@"C:\test.TXT") },
-					{ false, new FileInfo(@"c:\test.txt"), new FileInfo(@"test.txt") },
-					{ false, new FileInfo(@"c:\test.txt"), new FileInfo(@"c:\testDir\test.txt") },
-					{ false, new FileInfo(@"c:\test.txt"), null }
+					{ true,  new FileInfo(@"c:\test.txt"),   new FileInfo(@"c:\test.txt") },
+					{ true,  new FileInfo(@"c:\test.txt"),   new FileInfo(@"C:\test.TXT") },
+                    { true,  new DirectoryInfo(@"c:\test\"), new DirectoryInfo(@"C:\TEST\") },
+					{ false, new FileInfo(@"c:\test.txt"),   new FileInfo(@"test.txt") },
+					{ false, new FileInfo(@"c:\test.txt"),   new FileInfo(@"c:\testDir\test.txt") },
+					{ false, new FileInfo(@"c:\test.txt"),   null }
 				};
 			}
 		}
 
 		[Theory]
 		[PropertyData("EqualsData")]
-		public void Test_Equals(bool expected, FileInfo first, FileInfo second)
+        public void Test_Equals(bool expected, FileSystemInfo first, FileSystemInfo second)
 		{
 			// Act.
 			bool actual = comparer.Equals(first, second);
